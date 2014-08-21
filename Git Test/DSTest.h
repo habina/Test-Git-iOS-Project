b23cc19b33fd63ea6ca16f13de23d4517a53ccd2
Dasheng Chen <habinacds@gmail.com>
2014-08-21 12:59:26 -0700

    added a test class

---
+++ Git Test/DSTest.h
@@ -0,0 +1,13 @@
+//
+//  DSTest.h
+//  Git Test
+//
+//  Created by Dasheng Chen on 8/21/14.
+//  Copyright (c) 2014 CDS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSTest : NSObject
+
+@end
---
+++ Git Test/DSTest.m
@@ -0,0 +1,13 @@
+//
+//  DSTest.m
+//  Git Test
+//
+//  Created by Dasheng Chen on 8/21/14.
+//  Copyright (c) 2014 CDS. All rights reserved.
+//
+
+#import "DSTest.h"
+
+@implementation DSTest
+
+@end
